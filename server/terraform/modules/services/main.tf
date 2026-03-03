64dc1c6dffe4113d3fc49df4402fa265aa8c73a0
Vladyslav Kukharchuk <vladyslav.kukharchuk@makeheadway.com>
2026-03-03 20:39:23 +0200

    Draft 4

--- server/terraform/modules/services/main.tf
+++ server/terraform/modules/services/main.tf
@@ -12,3 +12,8 @@ resource "google_project_service" "firestore_api" {
   service            = "firestore.googleapis.com"
   disable_on_destroy = false
 }
+
+resource "google_project_service" "iam_credentials_api" {
+  service            = "iamcredentials.googleapis.com"
+  disable_on_destroy = false
+}
